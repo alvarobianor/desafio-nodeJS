ec6ad6d7253ba5c3f9d39b92393de5225414f75e
alvarobianor <bianor.alvaro12@gmail.com>
2020-04-08 01:37:36 -0300

    finished

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +10,74 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
+const indexValidation = (req, res, next) => {
+  const { id } = req.params;
+
+  const result = repositories.findIndex((e) => e.id === id);
+
+  result < 0 ? res.status(400).json({ error: 'error' }) : next();
+};
+
+app.get('/repositories', (request, response) => {
   // TODO
+  return response.send(repositories);
 });
 
-app.post("/repositories", (request, response) => {
+app.post('/repositories', (request, response) => {
   // TODO
+  const { techs, title, url } = request.body;
+
+  const repo = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repo);
+
+  return response.status(201).json(repo);
 });
 
-app.put("/repositories/:id", (request, response) => {
+app.put('/repositories/:id', indexValidation, (request, response) => {
   // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex((e) => e.id === id);
+  const { likes } = repositories[repoIndex];
+
+  repositories[repoIndex] = { id, title, url, techs, likes };
+
+  const newRepo = repositories[repoIndex];
+
+  return response.send(newRepo);
 });
 
-app.delete("/repositories/:id", (req, res) => {
+app.delete('/repositories/:id', indexValidation, (req, res) => {
   // TODO
-});
+  const { id } = req.params;
+  const repoIndex = repositories.findIndex((e) => e.id === id);
 
-app.post("/repositories/:id/like", (request, response) => {
+  if (repoIndex < 0) {
+    return res.status(400).send();
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return res.status(204).send();
+});
+app.use('/repositories/:id/like', indexValidation);
+app.post('/repositories/:id/like', (request, response) => {
   // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex((e) => e.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'id does not exist' });
+  }
+
+  repositories[repoIndex].likes += 1;
+
+  const { likes } = repositories[repoIndex];
+
+  return response.json({ likes });
 });
 
 module.exports = app;
